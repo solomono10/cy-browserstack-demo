098e331d2cf7800b5c12d59b7fdcf3c3f7971038
solomono <solomon_wuni@yahoo.co.uk>
2021-01-01 23:18:39 +0000

    add jenkinsfile9

--- script.sh
+++ script.sh
@@ -6,5 +6,5 @@ BROWSER=$3
 npm i
 npx cypress run --env PAGE=$PAGE \
                 --spec $FEATURE_FILES \
-                --browser $BROWSER
+                --browser $BROWSER \
                 --headless

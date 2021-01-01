88ea8a2c2c5ef6094c3b7758940eaa826d6756bf
solomono <solomon_wuni@yahoo.co.uk>
2021-01-01 23:09:32 +0000

    add jenkinsfile5

--- script.sh
+++ script.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 PAGE=$1
 FEATURE_FILES=$2
+BROWSER=$3
 
 npm i
 echo "Node version: $(node --version)"
-npx cypress run --env PAGE=$PAGE --spec $FEATURE_FILES
+npx cypress run --env \
+                PAGE=$PAGE \
+                --spec $FEATURE_FILES \
+                --browser $BROWSER

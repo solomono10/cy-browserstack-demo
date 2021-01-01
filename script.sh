575a58a2f07b0ade9292e1ae36d9b429e6041707
solomono <solomon_wuni@yahoo.co.uk>
2021-01-01 21:29:39 +0000

    update sh7

--- script.sh
+++ script.sh
@@ -5,6 +5,7 @@ FEATURE_FILES=$2
 echo "Hello $FIRSTNAME $LASTNAME"
 npm i
 echo "-----------------------"
-echo $(node --version)
+NODE_VERSION=$(node --version)
+echo "Node version is $(node --version)"
 echo "+++++++++++++++++++++++"
 CYPRESS_PAGE=$PAGE npx cypress run --spec $FEATURE_FILES
